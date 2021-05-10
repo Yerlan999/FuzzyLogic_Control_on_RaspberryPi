e5ea94da3f3d11c4b4cafa4e09c0e17adf62f2a7
YerlanAmir <fcbayernmunchenerlan@gmail.com>
2021-05-10 23:34:23 +0600

    Disk Control Algorith is Done and Ready to Test

---
+++ DiskControl.py
@@ -0,0 +1,43 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+
+z_current = ctrl.Antecedent(np.arange(-1, 1, 0.01), 'z_current')
+disk_voltage = ctrl.Consequent(np.arange(0, 1, 0.01), 'disk_voltage')
+
+disk_voltage.defuzzify_method = 'mom'
+
+# Custom membership functions can be built interactively with a familiar,
+z_current['-overload'] = fuzz.trapmf(z_current.universe,
+                                     [-1, -1, -0.90, -0.80])
+z_current['-nominal'] = fuzz.trapmf(z_current.universe,
+                                    [-0.90, -0.70, -0.20, -0.10])
+z_current['turned_off'] = fuzz.trapmf(
+    z_current.universe, [-0.20, -0.10, 0.10, 0.20])
+z_current['+nominal'] = fuzz.trapmf(z_current.universe,
+                                    [0.10, 0.20, 0.70, 0.90])
+z_current['+overload'] = fuzz.trapmf(z_current.universe, [0.80, 0.90, 1, 1])
+
+# Output membership functions
+disk_voltage['OFF'] = fuzz.trimf(disk_voltage.universe, [0, 0, 0.5])
+disk_voltage['ON'] = fuzz.trimf(disk_voltage.universe, [0.5, 1, 1])
+
+
+rule1 = ctrl.Rule(z_current['-overload'], disk_voltage['OFF'])
+rule2 = ctrl.Rule(z_current['-nominal'], disk_voltage['OFF'])
+rule3 = ctrl.Rule(z_current['turned_off'], disk_voltage['OFF'])
+rule4 = ctrl.Rule(z_current['+nominal'], disk_voltage['ON'])
+rule5 = ctrl.Rule(z_current['+overload'], disk_voltage['OFF'])
+
+
+disk_control = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
+disk = ctrl.ControlSystemSimulation(disk_control)
+
+
+def get_fuzzy_value(distance):
+
+    disk.input['z_current'] = distance
+    disk.compute()
+
+    return disk.output['disk_voltage']
---
+++ DiskControl_AlgorithmCombined.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import dc_motors
+import ultra_sonic
+import MotionControl
+from dc_motors import move_motor
+from ultra_sonic import get_distance
+from normalizing_funcs import custom_normalize
+from DiskControl import get_fuzzy_value
+
+
+# Setting up PIN modes
+GPIO.setmode(GPIO.BOARD)  # by physical PIN layout
+GPIO.setwarnings(False)  # disable warnings
+
+GPIO.setup(TRIGGER, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.setup(ENA, GPIO.OUT)
+GPIO.setup(ENB, GPIO.OUT)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            # Get distance value from Ultra Sonic Sensor
+            distance = get_distance()
+
+            # Normalize it to feed into Fuzzy Logic System
+            norm_dist = custom_normalize(distance, [0, 400], [0, 1])
+
+            # Feed into FLS
+            fuzz_val_mot = get_fuzzy_value(norm_dist)
+
+            # Normalize output values to match up with PWM range
+            norm_speed_mot = custom_normalize(fuzz_val_mot, [0, 1], [0, 100])
+
+            # Move motors according to Fuzzy Logic
+            move_motor(1, norm_speed_mot)
+
+            time.sleep(0.01)
+
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Process has been stopped by User")
+        GPIO.cleanup()
